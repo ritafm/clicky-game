147094b0c643acfb909c7c7add9de98b887938f8
Rita Manley <manley.rita@gmail.com>
2019-11-10 08:30:45 -0800

    Updates

---
+++ precache-manifest.28b5d36fef3781e31668e5e38e18ae46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c8c4f922760345ed380e7997a6757c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "88adad7967779161cee7",
+    "url": "/clicky-game/static/css/main.8a3976eb.chunk.css"
+  },
+  {
+    "revision": "9d10634c4ac3e8fb38e0",
+    "url": "/clicky-game/static/js/2.fc8f847a.chunk.js"
+  },
+  {
+    "revision": "88adad7967779161cee7",
+    "url": "/clicky-game/static/js/main.0181bdd7.chunk.js"
+  },
+  {
+    "revision": "00090092640f71911ff1",
+    "url": "/clicky-game/static/js/runtime-main.7285dd72.js"
+  }
+]);
